837cc93651161cf2b00435a2c26b2466e47ae5b0
ossor <odeniossor@gmail.com>
2024-01-25 11:24:23 +0300

    8

---
+++ Laba6/37 (Assert)/expressapp/webapp/operation.test.js
@@ -0,0 +1,9 @@
+
+var assert = require('assert');
+var operations = require('./operations');
+
+it('should multiply two numbers', function () {
+    var expected = 15;
+    var result = operations.multiply(3, 5);
+    assert.equal(result, expected);
+});
